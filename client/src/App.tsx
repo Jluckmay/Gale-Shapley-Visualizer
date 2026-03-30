bb2cb213ca8de8d8ade2e8af4d8b8aa0662266e9
João Lucas Mayrinck <55472486+Jluckmay@users.noreply.github.com>
2026-03-30 20:53:15 -0300

    Dark theme as default

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider
-        defaultTheme="light"
+        defaultTheme="dark"
         switchable
       >
         <TooltipProvider>
